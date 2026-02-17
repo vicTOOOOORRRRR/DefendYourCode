b6546d87133755418544c8e5005ae6c753b57a0a
vicTOOOOORRRRR <victort3828@gmail.com>
2026-02-16 23:07:42 -0800

    Added prompts for First/Last name and valid int inputs

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,61 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner input = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String firtName = getValidName(input, "FIRST");
+        String lastName = getValidName(input, "LAST");
+        int firstInt = getValidInt(input, "First Integer");
+        int secondInt = getValidInt(input, "Second Integer");
+
+
+    }
+    public static String getValidName(Scanner input, String type) {
+
+        String regex = "^[A-Z][a-z]{0,49}$";
+
+        while (true) {
+
+            System.out.println("Enter your " + type + " name:");
+            System.out.println("Requirements:");
+            System.out.println("- 1 to 50 letters");
+            System.out.println("- Letters only (A-Z or a-z)");
+            System.out.println("- Must start with uppercase letter");
+            System.out.println("Enter name here: ");
+            String name = input.nextLine();
+
+            if (name.matches(regex)) {
+                return name;
+            } else {
+                System.out.println("ERROR: Invalid " + type + " name format.\n");
+            }
+        }
+    }
+
+    public static int getValidInt(Scanner input, String label) {
+        while (true) {
+            System.out.println("Enter " + label);
+            System.out.println("Requirements:");
+            System.out.println("Must be 4-byte integer between -2147483648 to 2147483647");
+            System.out.println("Enter number here: ");
+            String line = input.nextLine();
+
+            try {
+                long temp = Long.parseLong(line);
+
+                if (temp < Integer.MIN_VALUE || temp > Integer.MAX_VALUE) {
+                    System.out.println("ERROR: Value out of 4-byte integer range.");
+                    continue;
+                }
+                return (int) temp;
+
+            } catch (NumberFormatException e) {
+                System.out.println("ERROR: You must enter a valid integer.");
+                //logError(e);
+            }
         }
     }
 }
